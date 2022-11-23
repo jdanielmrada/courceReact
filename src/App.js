7bd32747c2a69b0b89c38785607e07e25699fb87
Daniel <jdani9417@gmail.com>
2022-11-23 19:59:09 -0400

    Styles od component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const estilo2 = {
+    boxShadow: '0 5px 3px rgba(0,0,0,0.6)'
+}
+// ##################################
+const estilo = ({ bg = '#333', cl = '#999' }) => ({
+    backgroundColor: bg,
+    color: cl,
+    padding: '10px 15px',
+    margin: '10px 15px'
+})
+
+const Li = ({ children }) => {
+    console.log(children)
+    return (
+        <li style={{ ...estilo2, ...estilo({ bg: '#343', cl: '#fff' }) }} className='class-li'>{children}</li>
+    )
+}
+// tambien puede ser declarado como una
+// const App = () =>{}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const valor = 'feliz'
+    return (
+        <ul style={estilo({ bg: '#787' })} className='class-css'>
+            <Li estado="enojado">Valor del component</Li>
+        </ul>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Esta es la manera en la que se ve en the cource pero no funciona para la v-18
-// ReactDOM.render(
-//     'lala',
-//     document.getElementById('root')
-// )
-
-// <p>hola mundo</p> -> no recomendada
-// const x = React.createElement('p', null, 'hola mundo')
-//por buena practicas los component inician con mayuscula
-const Li = ({ children, estado, casa, edad }) => {
-    console.log(casa, edad)
-    return (<li>{children} esta {estado}</li>)
-}
-const X = () =>
-    <ul>
-        <Li
-            estado={'feliz'}
-            casa={false}
-            edad={20}
-        >Chanchito</Li>
-        <Li estado={'triste'}>Chanchito</Li>
-        <Li estado={'enojado'}>Chanchito</Li>
-    </ul>
-// Consegui hacer lo mismo fue utilizando las etiquetas de Ract
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // Recibe un jsx lo cual es lo mas optimo (React.createElemnt())
-    <X />
-
-
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
