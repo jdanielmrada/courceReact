e6fd04ee59cf5c5de8343638d7ccb40df79e8ca3
daniel mejias <jdani9417@gmail.com>
2022-12-04 19:57:48 -0400

    mount, update, unmount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import Button from "./button"
+import { Component } from "react"
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hola mundo</h1>
-            <Button onClick={() => console.log('clickead')}>
+class Button extends Component {
+    state = {}
+    constructor(props) {
+        super(props)
+        console.log('constructor', props);
+    }
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', prevProps, prevState)
+    }
+
+    componentWillUnmount() {
+        console.log('Desmontando Componente', this.props, this.state)
+    }
+    render() {
+        console.log('Renderizando button')
+        return (
+            <button onClick={() => this.setState({ prop: 1 })} >
                 Enviar
-            </Button>
-        </div>
-    )
+            </button>
+        )
+    }
+}
+class App extends Component {
+    state = {
+        valor: 3
+    }
+    render() {
+        console.log(this.state)
+        return (
+
+            <div>
+                <p>
+                    Hola mundo
+                </p>
+                {this.state.valor === 3
+                    ? <Button chanchito='Feliz' />
+                    : null
+                }
+                <button
+                    className={`${this.state.valor}`}
+                    onClick={() => this.setState({ valor: 2 })}>
+                    enviar en App
+                </button>
+            </div>
+        )
+    }
 }
 
 export default App
